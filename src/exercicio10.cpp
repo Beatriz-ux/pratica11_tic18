afee7e0348cb01214751da00a40abf95a64680c6
Beatriz <82838311+Beatriz-ux@users.noreply.github.com>
2023-09-19 21:40:58 -0300

    feat: questao finalizada

--- src/exercicio10.cpp
+++ src/exercicio10.cpp
@@ -1,5 +1,33 @@
 #include <iostream>
 using namespace std;
-int main(void){
+int verifica(char* texto,char letra, int* vetor){
+    
+    int cont=0;
+    int i=0;
+    while(texto[i] != '\0'){
+        if(texto[i]==letra){
+            vetor[i]=1;
+            cont++;
+        }
+        i++;
+    }
+    return cont;
+}
+int main() {
+    char texto[10];
+    char letra='a';
+    int vetor[10]={0},contador;
+    cout <<"Digite uma palavra: "<<endl;
+    cin >>texto;
+    cout <<"Digite uma letra: "<<endl;
+    cin >>letra;
+    
+    contador=verifica(texto,letra,vetor);
+    for(int i=0; i<(strlen(texto));i++){
+        if(vetor[i]==1){
+            cout << "Letra encontrada na posição: ["<<i+1<<"]" <<endl;
+        }
+    }
+    cout <<"Total de letras ("<<letra<<"): "<<contador;
     return 0;
 }
