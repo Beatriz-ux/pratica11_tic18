3e286e230d8e147e9675b2b4175f998c34cc449d
Beatriz <82838311+Beatriz-ux@users.noreply.github.com>
2023-09-19 21:45:20 -0300

    Fix: Concluido questão
    após tirar dúvidas sobre o retorno de um vetor, a solução foi deixar como void.

--- src/exercicio6.cpp
+++ src/exercicio6.cpp
@@ -1,21 +1,25 @@
 #include <iostream>
 using namespace std;
-int* intercala(int *vet1, int tam1, int *vet2, int tam2){
-    int vet3[9];
-    for (int i=0;i<tam1;i++){
-        vet3[i] = vet1[i];
+
+void intercala(int *vet1, int tam1, int *vet2, int tam2, int *resultado) {
+    for (int i = 0; i < tam1; i++) {
+        resultado[i] = vet1[i];
     }
-    for (int i=0;i<tam2;i++){
-        vet3[tam1+i] = vet2[i];
+    for (int i = 0; i < tam2; i++) {
+        resultado[tam1 + i] = vet2[i];
     }
-    return (int *)vet3;
 }
-int main(void){
-    int vetor1[5] = {1,2,3,4,5};
-    int vetor2[4] = {7,8,9,10};
-    int *vetor3;
-    vetor3 = intercala(vetor1, 5, vetor2, 4);
-    for(int i=0;i<9;i++){
+
+int main(void) {
+    const int tam1 = 5;
+    const int tam2 = 4;
+    int vetor1[tam1] = {1, 2, 3, 4, 5};
+    int vetor2[tam2] = {7, 8, 9, 10};
+    const int tam3 = tam1 + tam2;
+    int vetor3[tam3];
+    intercala(vetor1, tam1, vetor2, tam2, vetor3);
+
+    for (int i = 0; i < tam3; i++) {
         cout << vetor3[i] << " ";
     }
 
